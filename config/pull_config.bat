861dc209577bec6fc5b49d92b8e763f3675d7a7b
hxh <hxh19950701@gmail.com>
2026-04-25 11:05:03 +0800

    u

--- config/pull_config.bat
+++ config/pull_config.bat
@@ -1,11 +1,10 @@
 @echo off
 setlocal
 
-set "url=http://8.212.51.177/link/wmFAOi5Za7Puute1?clash=2"
+set "url=http://47.242.55.240/link/jjp2MZMNjjuO3ndo?clash=2"
 set "outfile=0dy.yaml"
-set "ua=clash.meta"
 
-powershell -Command "Invoke-WebRequest -Uri '%url%' -Headers @{ 'User-Agent' = '%ua%' } -OutFile '%outfile%'"
+curl -L -A "clash-verge/v2.4.7" -H "Accept: */*" "%url%" -o "%outfile%"
 
 echo Completed: %outfile%
 pause
