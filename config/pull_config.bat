b3ef9ba68e80fb9e1617f5983f536516e417df23
Vasthread Hu <hxh19950701@gmail.com>
2026-06-02 16:32:03 +0800

    Update URL in pull_config.bat

--- config/pull_config.bat
+++ config/pull_config.bat
@@ -1,7 +1,7 @@
 @echo off
 setlocal
 
-set "url=http://47.242.55.240/link/jjp2MZMNjjuO3ndo?clash=2"
+set "url=http://43.135.28.238/link/jjp2MZMNjjuO3ndo?clash=2"
 set "outfile=0dy.yaml"
 
 curl -L -A "clash-verge/v2.4.7" -H "Accept: */*" "%url%" -o "%outfile%"
